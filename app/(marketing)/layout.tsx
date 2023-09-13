fb5c4d1e8c795ea0d7627c842a7af3b3dba6b3dd
Theanh0411 <theanh@mogul.sg>
2023-09-13 16:36:07 +0700

    chore: stick header on top

--- app/(marketing)/layout.tsx
+++ app/(marketing)/layout.tsx
@@ -15,20 +15,22 @@ export default async function MarketingLayout({
 }: MarketingLayoutProps) {
   return (
     <div className="flex min-h-screen flex-col">
-      <header className="container z-40 bg-background">
-        <div className="flex h-20 items-center justify-between py-6">
-          <MainNav items={marketingConfig.mainNav} />
-          <nav>
-            <Link
-              href="/login"
-              className={cn(
-                buttonVariants({ variant: "secondary", size: "sm" }),
-                "px-4"
-              )}
-            >
-              Login
-            </Link>
-          </nav>
+      <header className="w-screen fixed bg-background shadow-sm z-100">
+        <div className="container ">
+          <div className="flex h-20 items-center justify-between py-6">
+            <MainNav items={marketingConfig.mainNav} />
+            <nav>
+              <Link
+                href="/login"
+                className={cn(
+                  buttonVariants({ variant: "secondary", size: "sm" }),
+                  "px-4"
+                )}
+              >
+                Login
+              </Link>
+            </nav>
+          </div>
         </div>
       </header>
       <main className="flex-1">{children}</main>
