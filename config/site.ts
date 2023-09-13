143f09a7078f5807a1d037c47d01ccb69426092a
Theanh0411 <theanh@mogul.sg>
2023-09-13 16:07:32 +0700

    Init project

---
+++ config/site.ts
@@ -0,0 +1,13 @@
+import { SiteConfig } from "types"
+
+export const siteConfig: SiteConfig = {
+  name: "Sophie",
+  description:
+    "An flashcard application with virtual assistant",
+  url: "https://tx.shadcn.com",
+  ogImage: "https://tx.shadcn.com/og.jpg",
+  links: {
+    twitter: "https://twitter.com/shadcn",
+    github: "https://github.com/shadcn/taxonomy",
+  },
+}
